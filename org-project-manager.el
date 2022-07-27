1284ef155e168d0f41b38e5ba02c80ed3e0020a9
binarydigitz01 <binarydigitz01@protonmail.com>
2022-07-27 07:19:41 +0530

    Updated comments and README

--- org-project-manager.el
+++ org-project-manager.el
@@ -6,16 +6,33 @@
 ;; Maintainer: Arnav Vijaywargiya <binarydigitz01@protonmail.com>
 ;; Created: March 06, 2022
 ;; Modified: March 06, 2022
-;; Version: 0.0.1
+;; Version: 1.0
 ;; Homepage: https://github.com/Ice-Cube69/org-project-manager
 ;; Package-Requires: ((emacs "24.3") (org-roam "2.2.2"))
 ;;
-;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 ;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;; This file is not part of GNU Emacs.
+
 ;;; Commentary:
 ;;
-;;  Description
-;;
+;; This module provides `org-project-manager-open-node'.
+`org-project-manager-delete' and `org-project-manager-capture-current', for use.
+
 ;;; Code:
 
 (require 'org-roam)
@@ -87,6 +104,7 @@
 
 (add-hook 'kill-emacs-hook #'org-project-manager-write-to-file)
 
+;;;###autoload
 (defun org-project-manager-delete ()
 	"Deletes all cache."
 	(interactive)
